b1bbf58b8fbba9fce130dc11617f3e18751f0559
Yeo Zhi Yuan <155881277+zyyye@users.noreply.github.com>
2026-06-04 14:58:33 +0800

    Add: longest-consecutive-sequence - submission-8

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-8.py
@@ -0,0 +1,42 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        if not nums: 
+            return 0
+
+        nums = list(set(nums))
+        def findSet(x):
+            if parent[x] == x:
+                return x
+            else:
+                parent[x] = findSet(parent[x])
+                return parent[x]
+        
+        def isSameSet(i, j):
+            return findSet(i) == findSet(j)
+
+        def unionSet(i, j):
+            if not isSameSet(i, j):
+                x, y = findSet(i), findSet(j)
+                if rank[x] > rank[y]:
+                    parent[y] = x
+                else:
+                    parent[x] = y
+                    if rank[x]==rank[y]: 
+                        rank[y] = rank[y] + 1 
+
+        parent = list(range(len(nums)))
+        rank = [0] * len(nums)
+
+        dct = {}
+        for i, j in enumerate(nums):
+            dct[j] = i
+        for num in nums:
+            if num + 1 in dct:
+                unionSet(dct[num + 1], dct[num])
+
+        for i in range(len(nums)):
+            findSet(i)
+
+        from collections import Counter
+        return max(Counter(parent).values())
+            
